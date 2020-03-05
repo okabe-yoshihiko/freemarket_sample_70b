353d678196d91235b907921bbf5606d580dcc235
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-05 14:06:52 +0900

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.12.0"
+
+set :application, "freemarket_sample_70b"
+set :repo_url, "git@github.com:okabe-yoshihiko/freemarket_sample_70b.git"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/freemarket_70b.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
