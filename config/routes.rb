e44d3d64bbcc757859886004309fa471f7c2832e
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-11 15:40:10 +0900

    confirm-change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :show, :confirm ]
+  resources :items, only: [:index, :show, :confirm ]do
+    collection do
+      get 'confirm', to: 'items#confirm'
+      post 'pay', to: 'items#pay'
+      get 'done', to: 'itemsdone'
+    end
+  end
   resources :users
   resources :cards, only: [:new, :create, :show] do
     collection do
