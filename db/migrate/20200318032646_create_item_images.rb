dd6d9092112456720b54ef1ebd0afb5fd804fd7c
ksean2124 <60636102+ksean2124@users.noreply.github.com>
2020-03-19 21:40:32 +0900

    新規登録　修正２

--- db/migrate/20200318032646_create_item_images.rb
+++
@@ -1,8 +0,0 @@
-class CreateItemImages < ActiveRecord::Migration[5.2]
-  def change
-    create_table :item_images do |t|
-      t.string :item_image, null: false
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_031649) do
+ActiveRecord::Schema.define(version: 2020_03_18_032118) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_031649) do
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
