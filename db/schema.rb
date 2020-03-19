85448dfd18ef32805854cc79f51d175ed8589707
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-19 20:39:39 +0900

    change

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_031649) do
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
