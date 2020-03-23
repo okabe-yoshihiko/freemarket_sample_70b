9bac234935b81c428f44f15b728322ce99432248
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-23 21:35:17 +0900

    あ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  #include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -30,8 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  include CarrierWave::MiniMagick
-  process resize_to_fit: [300, 300]
+   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -45,9 +44,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  if Rails.env.development? || Rails.env.test? 
-    storage :file
-  else
-    storage :fog
-  end
 end
