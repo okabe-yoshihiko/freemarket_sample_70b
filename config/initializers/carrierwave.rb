ea9c268f2092d7e5be56d7897ac1ae5eed612944
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-23 19:47:56 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,20 @@
-# CarrierWaveの設定呼び出し
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-# 保存先の分岐
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境:AWS使用
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1' # 東京はap-northeast-1
+      provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id], #credentails.ymlに鍵の本体があります
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],  #credentails.ymlに鍵の本体があります
+      region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'freemarket70b' #S3のバケット名
+    config.fog_directory  = freemarket70b
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket70b'
-  else
-    config.storage :file # 開発環境:public/uploades下に保存
-    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end  
+  end
 end
