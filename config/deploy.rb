c8ac224c476e7f859ae69df77541561fa5bd6a69
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-23 19:06:28 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ namespace :deploy do
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/scredentials.yml', "#{shared_path}/config/credentials.yml")
+      upload!('config/credentials.yml', "#{shared_path}/config/credentials.yml")
     end
   end
   before :starting, 'deploy:upload'
