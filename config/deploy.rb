ecca079114bfcb1896c2522aee27d23a1ffb6fff
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-24 15:33:53 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url,  'git@github.com:okabe-yoshihiko/freemarket_sample_70b.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %w{config/master.key}
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -28,28 +28,7 @@ set :keep_releases, 5
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
- task :restart do
-   invoke 'unicorn:restart'
- end
-
- desc 'upload master.key'
- task :upload do
-   on roles(:app) do |host|
-     if test "[ ! -d #{shared_path}/config ]"
-       execute "mkdir -p #{shared_path}/config"
-     end
-     upload!('config/master.key', "#{shared_path}/config/master.key")
-   end
- end
- before :starting, 'deploy:upload'
- after :finishing, 'deploy:cleanup'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 end
-
-# 必要に応じて/環境変数をcapistranoでの自動デプロイで利用
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
