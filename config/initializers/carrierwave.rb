9b81ef642af856d3d1a61b98f9f3db9b358f7d03
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-24 16:51:47 +0900

    aaa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,10 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.production?
   config.storage = :fog
+  config.fog_directory  = 'freemarket70b'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket70b'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -11,12 +14,8 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-    case Rails.env
-    when 'development'
-      　config.storage :file
-      　config.enable_processing = false if Rails.env.test?
-    when 'production'
-        config.fog_directory  = 'freemarket70b'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket70b'
-    end
+else
+  config.storage :file
+  config.enable_processing = false if Rails.env.test?
+end
 end
